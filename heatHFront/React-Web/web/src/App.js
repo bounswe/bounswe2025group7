9dd109bbf07cc91bc9100539c6a8ba51c87c674c
Kerem Bozkurt <kerembozkurt2002@gmail.com>
2025-04-25 01:41:08 +0300

    [UI]: SignIn Page is added which is similar to SignUp.

--- heatHFront/React-Web/web/src/App.js
+++ heatHFront/React-Web/web/src/App.js
@@ -3,15 +3,16 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from '@mui/material/styles';
 import theme from './theme/theme'; 
-import SignupPage from './pages/SignupPage';
+import SignupPage from './pages/Login/SignupPage.jsx';
+import SignIn from './pages/Login/SignIn.tsx';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <BrowserRouter>
         <Routes>
+        <Route path="/" element={<SignIn />} />
           <Route path="/signup" element={<SignupPage />} />
-
         </Routes>
       </BrowserRouter>
     </ThemeProvider>  
