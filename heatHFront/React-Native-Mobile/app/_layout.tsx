039d7ca7581719659f37ce999683bedb6a83c325
Amin Abu-Hilga <amin.abuhilga.140705@gmail.com>
2025-05-02 22:27:28 +0300

    AuthForm added

--- heatHFront/React-Native-Mobile/app/_layout.tsx
+++ heatHFront/React-Native-Mobile/app/_layout.tsx
@@ -1,9 +1,17 @@
-import { Stack } from "expo-router";
+// app/_layout.js
+import React from 'react';
+import { Stack } from 'expo-router';
+import { StatusBar } from 'react-native';
 
 export default function RootLayout() {
   return (
-    <Stack screenOptions={{headerShown: false}}>
-      <Stack.Screen name = 'auth/login'/>
-    </Stack>
-  )
+    <>
+      <StatusBar barStyle = "light-content"/>
+      <Stack screenOptions={{ headerShown: false }}>
+        {/* Ensure login is the first screen */}
+        <Stack.Screen name= "auth/login" options = {{}}/>
+        <Stack.Screen name= "auth/register" options = {{}}/>
+      </Stack>
+    </>
+  );
 }
--- heatHFront/React-Native-Mobile/components/auth/ThemedAuth.jsx
+++ heatHFront/React-Native-Mobile/components/auth/ThemedAuth.jsx
@@ -1,3 +1,4 @@
+import {useState} from 'react'
 import {View, StyleSheet, useColorScheme, Text, TextInput, Pressable} from 'react-native'
 import {Link} from 'expo-router'
 import {AuthColors} from '../../styles/Colors'
@@ -24,39 +25,56 @@ export const AuthInput = ({style, placeholder = 'email', ...props}) => {
     const theme = AuthColors[useColorScheme() ?? 'light'];
     
     return (
-        <TextInput placeholder = {placeholder} style = {[style, theme.authInput]} placeholderTextColor= {'white'} {...props}/>
+        <TextInput placeholder = {placeholder} style = {[style, theme.authInput]} placeholderTextColor= {'white'}  autoCapitalize = "none" {...props}/>
     )
 }
 
-export const AuthForm = ({type = 'Login'}) => {
+export const AuthForm = ({type = 'login'}) => {
     const theme = AuthColors[useColorScheme() ?? 'light'];
+
+    const handleSubmit = () => {
+        console.log('Data submitted: Email: ', email, ' Password: ', password);
+    }
+
+
+    const [email, setEmail] = useState('');
+    const [login, setLogin] = useState('');
+    const [password, setPassword] = useState('');
 
     return (    
         <AuthContainer>
             <Spacer/>
             <Text style = {theme.authTitle}>
-                Login
+                {(type === 'login') ? 'Login' : 'Register'}
             </Text>
 
             <Spacer height = {13}/>
-            <Text style = {{color: '#C0B79B'}}>
-                    Don't have an account yet ?  
+            <Text style = {{color: '#D2CBC2'}}>
+                    {(type === 'login') ? "Don't have an account yet ?" : "Already have an account ?"}
                 <Text> </Text>
-                <Link href = '/'>
-                    <Text style = {{color: '#CDA127', textDecorationLine: 'underline'}}>
-                        Sign Up
+                <Link href = 'auth/register'>
+                    <Text style = {{color: '#FBCB27', textDecorationLine: 'underline'}}>
+                        {(type === 'login') ? "Sign Up" : "Sign In"}
                     </Text>
                 </Link>
             </Text>
 
             <Spacer/>
-            <AuthInput placeholder = {type}/>
+            <AuthInput onChangeText = {setEmail} value = {email} placeholder = {(type === 'login') ? 'Login or Email' : 'Email'} />
+            
+            {
+                (type === 'register') ? (
+                <>
+                    <Spacer/> 
+                    <AuthInput onChangeText = {setLogin} value = {login} placeholder = 'Login' /> 
+                </> ) : null
+            }
 
             <Spacer/>
-            <AuthInput placeholder = 'Password'/>
+            <AuthInput onChangeText = {setPassword} value = {password} placeholder = 'Password'/>
 
             <Spacer height = {30}/>
-            <Pressable style = {({pressed}) => [theme.authButton, pressed && theme.authButtonPressed]}>
+            <Pressable onPress = {handleSubmit} style = {({pressed}) => [theme.authButton, pressed && theme.authButtonPressed]}>
                 <Text style = {theme.authButtonText}>
                     Login
                 </Text>
