0bac068ca92d5b1838617cca19a00fd01dc53191
burakorkmaz <kkorkmazburakk@gmail.com>
2025-05-11 00:27:23 +0300

    Backend : change path for vm configurtions

--- heatHBack/src/main/java/heatH/heatHBack/config/GcsConfig.java
+++ heatHBack/src/main/java/heatH/heatHBack/config/GcsConfig.java
@@ -5,18 +5,20 @@ import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.StorageOptions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 @Configuration
 public class GcsConfig {
 
     @Bean
-    public Storage storage() throws IOException {
+    public Storage storage() throws Exception {
+        InputStream credentialsStream = new ClassPathResource("gcp-connection.json").getInputStream();
         return StorageOptions.newBuilder()
-                .setCredentials(ServiceAccountCredentials.fromStream(
-                        new FileInputStream("")))
+                .setCredentials(ServiceAccountCredentials.fromStream(credentialsStream))
                 .build()
                 .getService();
     }
