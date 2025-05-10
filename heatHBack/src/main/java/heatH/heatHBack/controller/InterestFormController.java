ef7acef9e6cd9f0a411930657a4807de3f8cdf32
burakorkmaz <kkorkmazburakk@gmail.com>
2025-05-10 18:06:53 +0300

    Backend : Add edit form function to edit the user form. Users can update their features.

--- heatHBack/src/main/java/heatH/heatHBack/controller/InterestFormController.java
+++ heatHBack/src/main/java/heatH/heatHBack/controller/InterestFormController.java
@@ -1,12 +1,7 @@
 package heatH.heatHBack.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import heatH.heatHBack.model.InterestForm;
 import heatH.heatHBack.model.request.InterestFormRequest;
@@ -34,5 +29,11 @@ public class InterestFormController {
     public ResponseEntity<Boolean> checkFirstLogin() {
         Boolean isFirstLogin = interestFormService.checkFirstLogin();
         return ResponseEntity.ok(isFirstLogin);
+    }
+
+    @PutMapping("/update-form")
+    public ResponseEntity<InterestForm> updateForm(@RequestBody InterestFormRequest request) {
+        InterestForm updatedForm = interestFormService.updateForm(request);
+        return ResponseEntity.ok(updatedForm);
     }
 }
