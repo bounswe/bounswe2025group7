8a1c21f860c79131418cb3387d2e32310b8c24fe
burakorkmaz <kkorkmazburakk@gmail.com>
2025-05-14 07:45:04 +0300

    [Backend] : Add deletion to feed and recipe

--- heatHBack/src/main/java/heatH/heatHBack/repository/LikeRepository.java
+++ heatHBack/src/main/java/heatH/heatHBack/repository/LikeRepository.java
@@ -1,13 +1,21 @@
 package heatH.heatHBack.repository;
 
+import heatH.heatHBack.model.Feed;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import heatH.heatHBack.model.Like;
 import heatH.heatHBack.model.User;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface LikeRepository extends JpaRepository<Like, Long> {
     Optional<Like> findByUserAndFeedId(User user, Long feedId);
     long countByFeedId(Long feedId);
+    @Modifying
+    @Query("DELETE FROM Like l WHERE l.feed IN :feeds")
+    void deleteAllByFeedIn(@Param("feeds") List<Feed> feeds);
 }
