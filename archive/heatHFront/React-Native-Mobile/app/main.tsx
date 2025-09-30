2af4e455f34ca4024e73fb4514195ce4200d7386
burakorkmaz <kkorkmazburakk@gmail.com>
2025-09-30 15:26:51 +0300

    move projects to the main directory

--- archive/heatHFront/React-Native-Mobile/app/main.tsx
+++
@@ -1,1 +0,0 @@
-import {Image, View, ImageBackground, StyleSheet, SafeAreaView, Text} from 'react-native'
--- archive/heatHFront/React-Native-Mobile/components/general/Spacer.jsx
+++
@@ -1,9 +0,0 @@
-import {View} from 'react-native'
-
-const Spacer = ({width = 20, height = 20}) => {
-    return (
-        <View style = {{width: width, height: height}}/>
-    )
-}
-
-export default Spacer
