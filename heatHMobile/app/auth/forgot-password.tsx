8c4718309513d6fbfc7e148d5f1fdb8caa082283
poyrazogluyigit <poyrazogluyigit@gmail.com>
2025-10-04 16:05:14 +0300

    skeleton forgot/reset password pages; no backend connection

--- heatHMobile/app/auth/forgot-password.tsx
+++ heatHMobile/app/auth/forgot-password.tsx
@@ -1,11 +1,30 @@
 import { ThemedText } from '@/components/themed-text';
 import { ThemedView } from '@/components/themed-view';
+import Button from '@/components/ui/Button'
+import {Platform, TextInput, KeyboardAvoidingView, ScrollView} from "react-native";
+import {useState} from "react";
 
 export default function ForgotPasswordScreen() {
-  return (
-    <ThemedView>
-      <ThemedText>Forgot Password</ThemedText>
-    </ThemedView>
+
+    const [email, setEmail] = useState('');
+
+    return (
+        <KeyboardAvoidingView
+            behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+            <ScrollView>
+                <ThemedView style={{padding: 20}}>
+                    <ThemedText>Forgot Password</ThemedText>
+                    <TextInput
+                        placeholder={"Email address*"}
+                        value={email}
+                        onChangeText={setEmail}
+                    />
+                    <Button title={"Send Verification Code"}/>
+                    <Button title={"Back to Sign in"}/>
+                </ThemedView>
+            </ScrollView>
+        </KeyboardAvoidingView>
+
   );
 }
 
--- heatHMobile/app/auth/reset-password.tsx
+++ heatHMobile/app/auth/reset-password.tsx
@@ -1,12 +1,43 @@
 import { ThemedText } from '@/components/themed-text';
 import { ThemedView } from '@/components/themed-view';
+import Button from '@/components/ui/Button'
+import {Platform, TextInput, KeyboardAvoidingView, ScrollView} from "react-native";
+import {useState} from "react";
 
 export default function ResetPasswordScreen() {
-  return (
-    <ThemedView>
-      <ThemedText>Reset Password</ThemedText>
-    </ThemedView>
-  );
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+
+    return (
+        <KeyboardAvoidingView
+            behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+            <ScrollView>
+                <ThemedView style={{padding: 20}}>
+                    <ThemedText>Forgot Password</ThemedText>
+                    <TextInput
+                        placeholder={"Email address*"}
+                        value={email}
+                        onChangeText={setEmail}
+                    />
+                    <TextInput
+                        placeholder={"Password"}
+                        value={password}
+                        onChangeText={setPassword}
+                        />
+                    <TextInput
+                        placeholder={"Confirm Password"}
+                        value={confirmPassword}
+                        onChangeText={setConfirmPassword}
+                        />
+                    <Button title={"Reset Password"}/>
+                    <Button title={"Cancel"}/>
+                </ThemedView>
+            </ScrollView>
+        </KeyboardAvoidingView>
+
+    );
 }
 
 
